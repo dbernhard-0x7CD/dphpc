84ded7847e51335f2d90d7d7fa8d40e111034042
dbernhard <dbernhard@ethz.ch>
2022-10-19 16:04:59 +0200

    No longer set banshee when sourcing env.sh

--- scripts/env.sh
+++ scripts/env.sh
@@ -8,9 +8,6 @@ export SIM=$SNITCH_ROOT/hw/system/snitch_cluster/bin/snitch_cluster.vlt
 # Used when building
 export TOOLCHAIN_LLVM_FILE=$SNITCH_ROOT/sw/cmake/toolchain-llvm.cmake
 
-export BANSHEE_BIN=$SNITCH_ROOT/sw/banshee/target/debug/banshee
-
 # Commands
-alias banshee="$BANSHEE_BIN"
 alias sim="$SIM"
 
