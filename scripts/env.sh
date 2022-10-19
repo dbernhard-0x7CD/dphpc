7d3c2e4d33d309e9fd823cf3561ef690718c1a96
dbernhard <dbernhard@ethz.ch>
2022-10-19 16:14:04 +0200

    Add aliases for building and running

--- scripts/env.sh
+++ scripts/env.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# sets some important variables for developing
-
+# sets some variables for developing
+export PROOT=`pwd`
 export SNITCH_ROOT=`pwd`/snitch
 export SIM=$SNITCH_ROOT/hw/system/snitch_cluster/bin/snitch_cluster.vlt
 
@@ -9,5 +9,8 @@ export SIM=$SNITCH_ROOT/hw/system/snitch_cluster/bin/snitch_cluster.vlt
 export TOOLCHAIN_LLVM_FILE=$SNITCH_ROOT/sw/cmake/toolchain-llvm.cmake
 
 # Commands
+alias proot="cd $PROOT"
 alias sim="$SIM"
+alias run="banshee --configuration $SNITCH_ROOT/sw/banshee/config/snitch_cluster.yaml -l"
+alias build="cd $PROOT/build && cmake -DCMAKE_TOOLCHAIN_FILE=$TOOLCHAIN_LLVM_FILE .. && cmake --build . -j"
 
