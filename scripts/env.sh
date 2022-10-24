d567e164b11da719ee8bdc5736ab428f903b7898
dbernhard <dbernhard@ethz.ch>
2022-10-24 16:17:22 +0200

    Move to project root if we failed to build in alias 'build'

--- scripts/env.sh
+++ scripts/env.sh
@@ -32,7 +32,7 @@ alias sim="$SIM"
 alias run='banshee --configuration $SNITCH_ROOT/sw/banshee/config/snitch_cluster.yaml -l'
 
 # Builds locally
-alias build='cd $PROOT/build && cmake -DCMAKE_TOOLCHAIN_FILE=$TOOLCHAIN_LLVM_FILE .. && cmake --build . -j'
+alias build='cd $PROOT/build && cmake -DCMAKE_TOOLCHAIN_FILE=$TOOLCHAIN_LLVM_FILE .. && cmake --build . -j || cd ..'
 
 # Builds using docker
 alias dbuild='docker run --rm -v $PROOT:/repo -w /repo --name snitch_build ghcr.io/pulp-platform/snitch /bin/bash ./container_build.sh build'
