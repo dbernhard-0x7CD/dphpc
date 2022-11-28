7e412b46d160e2b9761a83dc1b5d3619f769b583
Georgijs Vilums <georgijs.vilums@gmail.com>
2022-11-28 15:22:28 +0100

    implement maxpool stride

--- src/benchmark/benchmark_maxpool.c
+++ src/benchmark/benchmark_maxpool.c
@@ -6,11 +6,15 @@
 #include "benchmark.h"
 
 __attribute__((noinline))
-int maxpool_baseline(float *a, size_t n, size_t filter_size, float* result);
+int maxpool_baseline(float *a, size_t n, size_t filter_size, size_t stride, float* result);
 __attribute__((noinline))
-int maxpool_ssr(float *a, size_t n, size_t filter_size, float* result);
+int maxpool_ssr(float *a, size_t n, size_t filter_size, size_t stride, float* result);
 __attribute__((noinline))
-int maxpool_ssr_frep(float *a, size_t n, size_t filter_size, float* result);
+int maxpool_ssr_frep(float *a, size_t n, size_t filter_size, size_t stride, float* result);
+
+size_t pool_output_size(size_t n, size_t filter_size, size_t stride);
+
+void print_pattern(float *a, size_t n, size_t filter_size, size_t stride, float* result);
 
 int main() {
     uint32_t core_idx = snrt_global_core_idx();
@@ -19,34 +23,30 @@ int main() {
     if (core_idx != 0) return 1;
 
     size_t filter_size = 5;
-    if (size % filter_size != 0) {
-        printf("ERROR: filter size %lu does not divide vector size %lu\n", filter_size, size);
-        return 1;
-    }
+    size_t stride = 2;
 
-    size_t result_size = size / filter_size;
+    size_t input_size = (size - 1) * stride + filter_size;
 
     // Initialize the input data
-    float* x = allocate(size, sizeof(float));
-    float* result_ref = allocate(result_size, sizeof(float));
-    float* result = allocate(result_size, sizeof(float));
+    float* x = allocate(input_size, sizeof(float));
+    float* result_ref = allocate(size, sizeof(float));
+    float* result = allocate(size, sizeof(float));
 
     for (size_t i = 0; i < size; i++) {
         x[i] = (float)i;
     }
 
-    
-    BENCH_VO(maxpool_baseline, x, size, filter_size, result_ref);
+    BENCH_VO(maxpool_baseline, x, input_size, filter_size, stride, result_ref);
 
 
-    BENCH_VO(maxpool_ssr, x, size, filter_size, result);
-    verify_vector(result, result_ref, result_size);
-    clear_vector(result, result_size);
+    BENCH_VO(maxpool_ssr, x, input_size, filter_size, stride, result);
+    verify_vector(result, result_ref, size);
+    clear_vector(result, size);
 
     // verify for this fails if the above "batchnorm_ssr" is executed
-    BENCH_VO(maxpool_ssr_frep, x, size, filter_size, result);
-    verify_vector(result, result_ref, result_size);
-    clear_vector(result, result_size);
+    BENCH_VO(maxpool_ssr_frep, x, input_size, filter_size, stride, result);
+    verify_vector(result, result_ref, size);
+    clear_vector(result, size);
 
  
     return 0;
--- src/onnx/maxpool.c
+++ src/onnx/maxpool.c
@@ -3,13 +3,17 @@
 
 #include "lmq.h"
 
+size_t pool_output_size(size_t n, size_t filter_size, size_t stride) {
+    return 1 + (n - filter_size) / stride;
+}
 
 __attribute__((noinline))
-int maxpool_baseline(float *a, size_t n, size_t filter_size, float* result) {
-    for (size_t i = 0; filter_size * i < n; ++i) {
-        float max = a[filter_size * i];
-        for (int j = 1; j < filter_size; ++j) {
-            float val = a[filter_size * i + j];
+int maxpool_baseline(float *a, size_t n, size_t filter_size, size_t stride, float* result) {
+    size_t out_size = pool_output_size(n, filter_size, stride);
+    for (size_t i = 0; i < out_size; ++i) {
+        float max = a[stride * i];
+        for (size_t j = 1; j < filter_size; ++j) {
+            float val = a[stride * i + j];
             if (val > max) {
                 max = val;
             }
@@ -21,25 +25,26 @@ int maxpool_baseline(float *a, size_t n, size_t filter_size, float* result) {
 
 
 __attribute__((noinline))
-int maxpool_ssr(float *a, size_t n, size_t filter_size, float* result) {
-    snrt_ssr_loop_1d(SNRT_SSR_DM0, n, sizeof(*a));
+int maxpool_ssr(float *a, size_t n, size_t filter_size, size_t stride, float* result) {
+    size_t out_size = pool_output_size(n, filter_size, stride);
+    snrt_ssr_loop_2d(SNRT_SSR_DM0, filter_size, out_size, sizeof(*a), sizeof(*a) * stride);
     snrt_ssr_repeat(SNRT_SSR_DM0, 1);
-    snrt_ssr_read(SNRT_SSR_DM0, SNRT_SSR_1D, a);
+    snrt_ssr_read(SNRT_SSR_DM0, SNRT_SSR_2D, a);
 
-    snrt_ssr_loop_1d(SNRT_SSR_DM1, n, sizeof(*result));
+    snrt_ssr_loop_1d(SNRT_SSR_DM1, out_size, sizeof(*result));
     snrt_ssr_repeat(SNRT_SSR_DM1, 1);
     snrt_ssr_write(SNRT_SSR_DM1, SNRT_SSR_1D, result);
 
     snrt_ssr_enable();
 
-    for (size_t i = 0; filter_size * i < n; ++i) {
+    for (size_t i = 0; i < out_size; ++i) {
         asm volatile(
             "fmv.s ft2, ft0 \n"
             :
             :
             : "ft0", "ft2"
         );
-        for (int j = 1; j < filter_size; ++j) {
+        for (size_t j = 1; j < filter_size; ++j) {
             asm volatile(
                 "fmax.s ft2, ft2, ft0 \n"
                 :
@@ -60,28 +65,29 @@ int maxpool_ssr(float *a, size_t n, size_t filter_size, float* result) {
 }
 
 __attribute__((noinline))
-int maxpool_ssr_frep(float *a, size_t n, size_t filter_size, float* result) {
+int maxpool_ssr_frep(float *a, size_t n, size_t filter_size, size_t stride, float* result) {
+    size_t out_size = pool_output_size(n, filter_size, stride);
     if (filter_size == 1) {
         return 0;
     }
-    snrt_ssr_loop_1d(SNRT_SSR_DM0, n, sizeof(*a));
+    snrt_ssr_loop_2d(SNRT_SSR_DM0, filter_size, out_size, sizeof(*a), sizeof(*a) * stride);
     snrt_ssr_repeat(SNRT_SSR_DM0, 1);
-    snrt_ssr_read(SNRT_SSR_DM0, SNRT_SSR_1D, a);
+    snrt_ssr_read(SNRT_SSR_DM0, SNRT_SSR_2D, a);
 
-    snrt_ssr_loop_1d(SNRT_SSR_DM1, n, sizeof(*result));
+    snrt_ssr_loop_1d(SNRT_SSR_DM1, out_size, sizeof(*result));
     snrt_ssr_repeat(SNRT_SSR_DM1, 1);
     snrt_ssr_write(SNRT_SSR_DM1, SNRT_SSR_1D, result);
 
     snrt_ssr_enable();
 
-    for (size_t i = 0; filter_size * i < n; ++i) {
+    for (size_t i = 0; i < out_size; ++i) {
         asm volatile(
             "fmv.s ft2, ft0 \n"
             "frep.o %[n_frep], 1, 0, 0 \n"
             "fmax.s ft2, ft2, ft0 \n"
             "fmv.s ft1, ft2 \n"
             :
-            : [n_frep] "r"(filter_size - 2)
+            : [n_frep] "r"(filter_size - 2), [n_outer_frep] "r"(out_size - 1)
             : "ft0", "ft2"
         );
     }
