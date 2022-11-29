f30c371907d136817346c4e7eea2ab7ede2a4617
Markus Pobitzer <markuspobitzer@gmail.com>
2022-11-29 16:01:22 +0100

    Parallel add (with barriers)

--- src/onnx/add.h
+++ src/onnx/add.h
@@ -12,4 +12,8 @@ int add_omp(float *a, float *b, const size_t n, float *result);
 int add_ssr_omp(float *a, float *b, const size_t n, float *result);
 int add_ssr_frep_omp(float *a, float *b, const size_t n, float *result);
 
+int add_parallel(float *a, float *b, const size_t n, float *result);
+int add_ssr_parallel(float *a, float *b, const size_t n, float *result);
+int add_ssr_frep_parallel(float *a, float *b, const size_t n, float *result);
+
 #endif
