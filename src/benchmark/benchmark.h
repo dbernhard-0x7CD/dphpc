02350dfa17f5fad1a5903388c41087d2b31bf80b
dbernhard <dbernhard@ethz.ch>
2022-11-30 10:36:24 +0100

    Highlight if a mismatch happens when verifying

--- src/benchmark/benchmark.h
+++ src/benchmark/benchmark.h
@@ -62,7 +62,7 @@ size_t* cycles_count;
 static inline void verify_vector(const float* value, const float* reference, const size_t n) {
     for (size_t i = 0; i < n; ++i) {
         if (value[i] != reference[i]) {
-            printf("At i=%d: expected %.10f, but got %.10f\n", i, reference[i], value[i]);
+            printf("MISMATCH at i=%d: expected %.10f, but got %.10f\n", i, reference[i], value[i]);
         }
     }
 };
