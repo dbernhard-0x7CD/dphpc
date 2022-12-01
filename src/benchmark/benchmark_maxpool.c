86d87a9cb2839c9a6fee12c687699575cdd8ad13
Georgijs Vilums <georgijs.vilums@gmail.com>
2022-12-01 11:09:25 +0100

    implement maxpool2d

--- src/benchmark/benchmark_maxpool.c
+++ src/benchmark/benchmark_maxpool.c
@@ -12,9 +12,47 @@ int maxpool_ssr(float *a, size_t n, size_t filter_size, size_t stride, float* re
 __attribute__((noinline))
 int maxpool_ssr_frep(float *a, size_t n, size_t filter_size, size_t stride, float* result);
 
-size_t pool_output_size(size_t n, size_t filter_size, size_t stride);
+__attribute__((noinline))
+int maxpool2d_baseline(float *a, size_t n0, size_t n1, size_t f0, size_t f1, size_t s0, size_t s1, float* result);
+__attribute__((noinline))
+int maxpool2d_ssr(float *a, size_t n0, size_t n1, size_t f0, size_t f1, size_t s0, size_t s1, float* result);
+__attribute__((noinline))
+int maxpool2d_ssr_frep(float *a, size_t n0, size_t n1, size_t f0, size_t f1, size_t s0, size_t s1, float* result);
 
+size_t pool_output_size(size_t n, size_t filter_size, size_t stride);
 void print_pattern(float *a, size_t n, size_t filter_size, size_t stride, float* result);
+
+int run2d() {
+    size_t f0 = 5;
+    size_t f1 = 4;
+    size_t s0 = 3;
+    size_t s1 = 2;
+
+    size_t outn0 = 300;
+    size_t outn1 = 500;
+
+    size_t n0 = (outn0 - 1) * s0 + f0;
+    size_t n1 = (outn1 - 1) * s1 + f1;
+
+    float* x = allocate(n0 * n1, sizeof(float));
+    float* result_ref = allocate(outn0 * outn1, sizeof(float));
+    float* result = allocate(outn0 * outn1, sizeof(float));
+
+    for (size_t i = 0; i < n0 * n1; i++) {
+        x[i] = (float)i;
+    }
+
+    printf("real size: %lu\n", n0 * n1);
+    BENCH_VO(maxpool2d_baseline, x, n0, n1, f0, f1, s0, s1, result_ref);
+
+    BENCH_VO(maxpool2d_ssr, x, n0, n1, f0, f1, s0, s1, result);
+    verify_vector(result, result_ref, outn0 * outn1);
+    clear_vector(result, outn0 * outn1);
+
+    BENCH_VO(maxpool2d_ssr_frep, x, n0, n1, f0, f1, s0, s1, result);
+    verify_vector(result, result_ref, outn0 * outn1);
+    clear_vector(result, outn0 * outn1);
+}
 
 int main() {
     uint32_t core_idx = snrt_global_core_idx();
@@ -32,7 +70,7 @@ int main() {
     float* result_ref = allocate(size, sizeof(float));
     float* result = allocate(size, sizeof(float));
 
-    for (size_t i = 0; i < size; i++) {
+    for (size_t i = 0; i < input_size; i++) {
         x[i] = (float)i;
     }
 
@@ -43,12 +81,14 @@ int main() {
     verify_vector(result, result_ref, size);
     clear_vector(result, size);
 
-    // verify for this fails if the above "batchnorm_ssr" is executed
     BENCH_VO(maxpool_ssr_frep, x, input_size, filter_size, stride, result);
     verify_vector(result, result_ref, size);
     clear_vector(result, size);
 
- 
+    run2d();
+
     return 0;
 }
+
+
 
--- src/onnx/maxpool.c
+++ src/onnx/maxpool.c
@@ -95,3 +95,103 @@ int maxpool_ssr_frep(float *a, size_t n, size_t filter_size, size_t stride, floa
     snrt_ssr_disable();
     return 0;
 }
+
+__attribute__((noinline))
+int maxpool2d_baseline(float *a, size_t n0, size_t n1, size_t f0, size_t f1, size_t s0, size_t s1, float* result) {
+    size_t outn0 = pool_output_size(n0, f0, s0);
+    size_t outn1 = pool_output_size(n1, f1, s1);
+
+    for (size_t i = 0; i < outn1; ++i) {
+        for (size_t j = 0; j < outn0; ++j) {
+            float max = a[n0 * s1 * i + s0 * j];
+            for (size_t k = 0; k < f1; ++k) {
+                for (size_t l = 0; l < f0; ++l) {
+                    float val = a[n0 * (s1 * i + k) + s0 * j + l];
+                    if (val > max) {
+                        max = val;
+                    }
+                }
+            }
+            result[i * outn0 + j] = max;
+        }
+    }
+    return 0;
+}
+
+
+__attribute__((noinline))
+int maxpool2d_ssr(float *a, size_t n0, size_t n1, size_t f0, size_t f1, size_t s0, size_t s1, float* result) {
+    size_t outn0 = pool_output_size(n0, f0, s0);
+    size_t outn1 = pool_output_size(n1, f1, s1);
+
+    snrt_ssr_loop_4d(SNRT_SSR_DM0, f0, f1, outn0, outn1, sizeof(*a), sizeof(*a) * n0, sizeof(*a) * s0, sizeof(*a) * s1 * n0);
+    snrt_ssr_repeat(SNRT_SSR_DM0, 1);
+    snrt_ssr_read(SNRT_SSR_DM0, SNRT_SSR_4D, a);
+
+    snrt_ssr_loop_1d(SNRT_SSR_DM1, outn0 * outn1, sizeof(*result));
+    snrt_ssr_repeat(SNRT_SSR_DM1, 1);
+    snrt_ssr_write(SNRT_SSR_DM1, SNRT_SSR_1D, result);
+
+    snrt_ssr_enable();
+
+    for (size_t i = 0; i < outn0 * outn1; ++i) {
+        asm volatile(
+            "fmv.s ft2, ft0 \n"
+            :
+            :
+            : "ft0", "ft2"
+        );
+        for (size_t k = 1; k < f0 * f1; ++k) {
+            asm volatile(
+                "fmax.s ft2, ft2, ft0 \n"
+                :
+                :
+                : "ft0", "ft2"
+            );
+        }
+        asm volatile(
+            "fmv.s ft1, ft2 \n"
+            :
+            :
+            : "ft1", "ft2"
+        );
+    }
+
+    snrt_ssr_disable();
+    return 0;
+}
+
+__attribute__((noinline))
+int maxpool2d_ssr_frep(float *a, size_t n0, size_t n1, size_t f0, size_t f1, size_t s0, size_t s1, float* result) {
+    if (f0 * f1 == 1) {
+        return 1;
+    }
+
+    size_t outn0 = pool_output_size(n0, f0, s0);
+    size_t outn1 = pool_output_size(n1, f1, s1);
+
+    snrt_ssr_loop_4d(SNRT_SSR_DM0, f0, f1, outn0, outn1, sizeof(*a), sizeof(*a) * n0, sizeof(*a) * s0, sizeof(*a) * s1 * n0);
+    snrt_ssr_repeat(SNRT_SSR_DM0, 1);
+    snrt_ssr_read(SNRT_SSR_DM0, SNRT_SSR_4D, a);
+
+    snrt_ssr_loop_1d(SNRT_SSR_DM1, outn0 * outn1, sizeof(*result));
+    snrt_ssr_repeat(SNRT_SSR_DM1, 1);
+    snrt_ssr_write(SNRT_SSR_DM1, SNRT_SSR_1D, result);
+
+    snrt_ssr_enable();
+
+    for (size_t i = 0; i < outn0 * outn1; ++i) {
+        asm volatile(
+            "fmv.s ft2, ft0 \n"
+            "frep.o %[n_frep], 1, 0, 0 \n"
+            "fmax.s ft2, ft2, ft0 \n"
+            "fmv.s ft1, ft2 \n"
+            :
+            : [n_frep] "r"(f0 * f1 - 2)
+            : "ft0", "ft2"
+        );
+    }
+
+    snrt_ssr_disable();
+    return 0;
+}
