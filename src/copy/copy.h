c5afdabec57703581bb149009d0d7bc2175ff56c
dbernhard <dbernhard@ethz.ch>
2022-12-06 19:08:56 +0100

    Formatting

--- src/copy/copy.h
+++ src/copy/copy.h
@@ -8,21 +8,15 @@
  * Naive implementation of copy. Copies n elements starting at source to target
  */
 int copy_baseline(float* source, const size_t n, float* target);
-
 int copy_ssr(float* source, const size_t n, float* target);
-
 int copy_ssr_frep(float* source, const size_t n, float* target);
 
 int copy_parallel(float* source, const size_t n, float* target);
-
 int copy_ssr_parallel(float* source, const size_t n, float* target);
-
 int copy_ssr_frep_parallel(float* source, const size_t n, float* target);
 
 int copy_omp(float* source, const size_t n, float* target);
-
 int copy_ssr_omp(float* source, const size_t n, float* target);
-
 int copy_ssr_frep_omp(float* source, const size_t n, float* target);
 
 #endif
