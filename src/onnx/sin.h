e4e5b784b128be1c0dbd0cdca5fb7c101df37981
dbernhard <dbernhard@ethz.ch>
2022-12-06 14:49:09 +0100

    Fix compilation 2

--- src/onnx/sin.h
+++ src/onnx/sin.h
@@ -22,7 +22,7 @@ int sin_baseline_lookup_table(float* arr, const size_t n, float* result, float* 
 
 int sin_ssr_lookup_table(float* arr, const size_t n, float* result, float* lookup_table, const size_t lookup_table_size);
 
-int sin_omp(volatile float* arr, const size_t n, float* result);
+int sin_omp(float* arr, const size_t n, float* result);
 
 int sin_ssr_omp(const float* arr, const size_t n, float* result);
 
