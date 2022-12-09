84d5ac80c8de241ed5bee3bb92233553917d0d9b
dbernhard <dbernhard@ethz.ch>
2022-12-09 14:14:16 +0100

    benchmark_add: Run deterministic part only once

--- src/benchmark/benchmark_add.c
+++ src/benchmark/benchmark_add.c
@@ -11,34 +11,38 @@ float *x, *y, *result_ref, *result;
 int main() {
     uint32_t core_idx = snrt_cluster_core_idx();
     uint32_t core_num = snrt_cluster_core_num() - 1; // -1 as there is one DM core
-    for(size_t size=LMQ_START_SIZE;size<=LMQ_SIZE;size*=2){
-        if (core_idx == 0){
-            // Initialize the input data
-            x = allocate(size, sizeof(float));
-            y = allocate(size, sizeof(float));
-            result_ref = allocate(size, sizeof(float));
-            result = allocate(size, sizeof(float));
-            cycles_count = allocate(core_num, sizeof(size_t));
 
-            for (unsigned i = 0; i < size; i++) {
-                x[i] = (float)i;
-                y[i] = (float)i;
-            }
-            
-            BENCH_VO(add_baseline, x, y, size, result_ref);
+    // benchmark ssr+frep on a single core
+    if (core_idx == 0) {
+        printf("Running benchmark_add\n");
 
-            BENCH_VO(add_ssr, x, y, size, result);
-            verify_vector(result, result_ref, size);
-            clear_vector(result, size);
+        // Initialize the input data
+        x = allocate(size, sizeof(float));
+        y = allocate(size, sizeof(float));
+        result_ref = allocate(size, sizeof(float));
+        result = allocate(size, sizeof(float));
+        cycles_count = allocate(core_num, sizeof(size_t));
 
-            BENCH_VO(add_ssr_frep, x, y, size, result);
-            verify_vector(result, result_ref, size);
-            clear_vector(result, size);
+        for (unsigned i = 0; i < size; i++) {
+            x[i] = (float)i;
+            y[i] = (float)i;
         }
-        snrt_global_barrier();
+        
+        BENCH_VO(add_baseline, x, y, size, result_ref);
 
+        BENCH_VO(add_ssr, x, y, size, result);
+        verify_vector(result, result_ref, size);
+        clear_vector(result, size);
 
-        // ======================= Parallel benchmarks with barriers ===========================
+        BENCH_VO(add_ssr_frep, x, y, size, result);
+        verify_vector(result, result_ref, size);
+        clear_vector(result, size);
+    }
+
+    snrt_cluster_hw_barrier();
+    /* Benchmark parallel */
+    for(size_t size=LMQ_START_SIZE;size<=LMQ_SIZE;size*=2){
+        // =========== Parallel benchmarks with barriers ===========
         size_t chunk_size = size / core_num;
         cycles_count[core_idx] = 0;
         BENCH_VO_PARALLEL(add_parallel, x, y, size, result);
@@ -46,7 +50,6 @@ int main() {
             verify_vector_omp(result, result_ref, size, chunk_size);
             clear_vector(result, size);
         }
-        snrt_global_barrier();
 
         cycles_count[core_idx] = 0;
         BENCH_VO_PARALLEL(add_ssr_parallel, x, y, size, result);
@@ -54,7 +57,6 @@ int main() {
             verify_vector_omp(result, result_ref, size, chunk_size);
             clear_vector(result, size);
         }
-        snrt_global_barrier();
 
         cycles_count[core_idx] = 0;
         BENCH_VO_PARALLEL(add_ssr_frep_parallel, x, y, size, result);
@@ -62,10 +64,9 @@ int main() {
             verify_vector_omp(result, result_ref, size, chunk_size);
             clear_vector(result, size);
         }
-        snrt_global_barrier();
     }
 
-    // ======================= Parallel benchmarks with OMP ===========================
+    // ======== Parallel benchmarks with OMP =================
     __snrt_omp_bootstrap(core_idx);
     for(size_t size=LMQ_START_SIZE;size<=LMQ_SIZE;size*=2){
         size_t chunk_size = size / core_num;
