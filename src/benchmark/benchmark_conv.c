99ca7fa3b3223929db5b5c9ffca082ca80fbdfbd
dbernhard <dbernhard@ethz.ch>
2022-12-13 10:17:43 +0100

    Remove dead code

--- src/benchmark/benchmark_conv.c
+++ src/benchmark/benchmark_conv.c
@@ -22,38 +22,6 @@ int conv2d_ssr_frep(float *a, float* filter, size_t n0, size_t n1, size_t f0, si
 size_t conv_output_size(size_t n, size_t filter_size, size_t stride, size_t dilation);
 // void print_pattern(float *a, size_t n, size_t filter_size, size_t stride, float* result);
 
-static int run2d() {
-    size_t f0 = 5;
-    size_t f1 = 4;
-    size_t s0 = 3;
-    size_t s1 = 2;
-
-    size_t outn0 = 300;
-    size_t outn1 = 500;
-
-    size_t n0 = (outn0 - 1) * s0 + f0;
-    size_t n1 = (outn1 - 1) * s1 + f1;
-
-    float* x = allocate(n0 * n1, sizeof(float));
-    float* result_ref = allocate(outn0 * outn1, sizeof(float));
-    float* result = allocate(outn0 * outn1, sizeof(float));
-
-    for (size_t i = 0; i < n0 * n1; i++) {
-        x[i] = (float)i;
-    }
-
-    printf("real size: %lu\n", n0 * n1);
-    // BENCH_VO(maxpool2d_baseline, x, n0, n1, f0, f1, s0, s1, result_ref);
-
-    // BENCH_VO(maxpool2d_ssr, x, n0, n1, f0, f1, s0, s1, result);
-    verify_vector(result, result_ref, outn0 * outn1);
-    clear_vector(result, outn0 * outn1);
-
-    // BENCH_VO(maxpool2d_ssr_frep, x, n0, n1, f0, f1, s0, s1, result);
-    verify_vector(result, result_ref, outn0 * outn1);
-    clear_vector(result, outn0 * outn1);
-}
-
 int main() {
     uint32_t core_idx = snrt_global_core_idx();
 
