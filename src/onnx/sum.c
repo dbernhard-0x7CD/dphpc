28bb8a78bbc7db162f2c82598af7a59cdcd836f5
dbernhard <dbernhard@ethz.ch>
2022-12-13 10:10:51 +0100

    Remove some unnecessary variables

--- src/onnx/sum.c
+++ src/onnx/sum.c
@@ -20,17 +20,13 @@ int sum_baseline(float *arr, const size_t n, float* result) {
 
 __attribute__((noinline)) 
 int sum_ssr(float *arr, const size_t n, float* result) {
-
-    register volatile float ft0 asm("ft0");
-    asm volatile("" : "=f"(ft0));
-
     snrt_ssr_loop_1d(SNRT_SSR_DM0, n, sizeof(*arr));
     snrt_ssr_repeat(SNRT_SSR_DM0, 1);
     snrt_ssr_read(SNRT_SSR_DM0, SNRT_SSR_1D, arr);
 
     snrt_ssr_enable();
 
-    register float s = 0;
+    volatile register float s = 0.0;
     for (size_t i = 0; i < n; i++) {
         asm volatile(
             "fadd.s %[s], ft0, %[s] \n"
@@ -39,7 +35,6 @@ int sum_ssr(float *arr, const size_t n, float* result) {
     }
 
     snrt_ssr_disable();
-    asm volatile("" :: "f"(ft0));
 
     *result = s;
 
@@ -48,10 +43,6 @@ int sum_ssr(float *arr, const size_t n, float* result) {
 
 __attribute__((noinline)) 
 int sum_ssr_frep(float *arr, const size_t n, float* result) {
-
-    register volatile float ft0 asm("ft0");
-    asm volatile("" : "=f"(ft0));
-
     snrt_ssr_loop_1d(SNRT_SSR_DM0, n, sizeof(*arr));
     snrt_ssr_repeat(SNRT_SSR_DM0, 1);
     snrt_ssr_read(SNRT_SSR_DM0, SNRT_SSR_1D, arr);
@@ -68,7 +59,6 @@ int sum_ssr_frep(float *arr, const size_t n, float* result) {
     );
 
     snrt_ssr_disable();
-    asm volatile("" :: "f"(ft0));
 
     *result = s;
 
