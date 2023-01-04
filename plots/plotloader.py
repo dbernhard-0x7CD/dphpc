a3ba6882f12be251473bacad638a969efabfc1e5
PapediPoo <rschmidiger64@gmail.com>
2023-01-04 19:37:42 +0100

    tested seaborn

--- plots/plotloader.py
+++ plots/plotloader.py
@@ -2,6 +2,7 @@ import argparse
 import os
 import json
 from itertools import compress
+import pandas as pd
 
 '''
 Loads all *.json files in abspath and returns a list of functions and a dictionary with all runtime-cycles.
@@ -42,6 +43,51 @@ def load_plot_data(abspath, include=[], exclude=[]):
     print("[    DATA LOADER]     loaded data for the plots {}".format(list(data.keys())))
     return functions, data
 
+def load_plot_dataframe(abspath, include=[], exclude=[]):
+    files = list()
+    data = dict()
+    functions = list()
+
+    assert(os.path.isabs(abspath))
+    # load all json files into list
+    for filename in os.listdir(abspath):
+        if filename.endswith(".json"):
+            try:
+                files = files + [json.load(open(abspath + filename, 'r'))]
+            except:
+                pass
+    
+    # merge all json dicts into one
+    for json_file in files:
+        for func_name in json_file.keys():
+            if func_name == "n": continue
+            functions.append(func_name)
+            if isinstance(json_file[func_name][0], list):
+                x = [{"n": int(ni), "cycles": int(ci)} for (ni, cii) in zip(json_file["n"], json_file[func_name]) for ci in cii]
+                data[func_name] = x
+            else:
+                x = [{"n": int(ni), "cycles": int(ci)} for (ni, ci) in zip(json_file["n"], json_file[func_name])]
+                data[func_name] = x
+    
+    # filter keys according to inclue/exclude
+    func_names = list(data.keys())
+    func_names = arg_filter(func_names, include, exclude)
+    tmp = []
+    # tmp["n"] = data["n"]
+    for fn in func_names:
+        if fn == "n": continue
+        tmp.append({"implementation name": fn, "data": data[fn]})
+    data = tmp
+
+    data = pd.json_normalize(
+    data,
+    record_path=['data'],
+    meta=['implementation name'],
+    errors='ignore')
+    
+    functions = arg_filter(functions, include, exclude)
+    print("[    DATA LOADER]     loaded data for the plots {}".format(func_names))
+    return functions, data
 
 def arg_parse():
     # add argparse to specify function names that should either be included or excluded
