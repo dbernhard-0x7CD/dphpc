d107cec4eecf942d7530be54b2daf6d85edd15dc
dbernhard <dbernhard@ethz.ch>
2023-01-11 13:50:10 +0100

    ssr_anomaly: Mention that it's only for float streams

--- src/bugs/ssr_anomaly.c
+++ src/bugs/ssr_anomaly.c
@@ -3,11 +3,11 @@
 
 #include "lmq.h"
 #include "snrt.h"
-#include "float.h"
 
 /*
- * This shows that SSR will write -inf at the end of every stream.
- *
+ * This shows that SSR will write -inf at the end of every float stream.
+ * This works for double stream.
+ * 
  * We write up to index 2 (so 3 write instructions) the value '0.0'.
  * We expect that the value at index 3 is untouched but it's '-inf'.
  */
@@ -28,13 +28,13 @@ int main() {
     // we have a value of -inf which somehow was also written.
     const size_t size = 10;
     const volatile uint32_t write_up_to = 4;
-    volatile double* x = allocate(size, sizeof(double));
+    volatile float* x = allocate(size, sizeof(float));
 
     for (size_t i = 0; i < size; i++) {
         x[i] = i;
     }
 
-    snrt_ssr_loop_1d(SNRT_SSR_DM0, write_up_to, sizeof(double));
+    snrt_ssr_loop_1d(SNRT_SSR_DM0, write_up_to, sizeof(float));
     snrt_ssr_repeat(SNRT_SSR_DM0, 1);
     snrt_ssr_write(SNRT_SSR_DM0, SNRT_SSR_1D, x);
 
@@ -42,15 +42,15 @@ int main() {
     for (uint32_t i = 0; i < write_up_to; i++) {
         asm volatile(
             "addi x5, zero, 0\n"    // x5 <-- 0
-            "fcvt.d.w ft1, x5\n"    // ft1 <-- double(x5)
-            "fmv.d ft0, ft1\n"      // ft0 <-- ft1 (write to x[i])
-            ::: "ft0", "ft1", "x5"
+            "fcvt.s.w ft1, x5\n"    // ft1 <-- double(x5)
+            "fmv.s ft0, ft1\n"      // ft0 <-- ft1 (write to x[i])
+            ::: "ft0", "ft1", "x5", "memory"
         );
     }
     snrt_ssr_disable();
 
     for (size_t i = 0; i < size; i++) {
-        printf("Value at %d is %lf\n", i, x[i]);
+        printf("Value at %d is %f\n", i, x[i]);
     }
 
     snrt_cluster_hw_barrier();
