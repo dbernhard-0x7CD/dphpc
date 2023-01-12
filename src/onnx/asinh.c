563196f0a8a3125900e60c3374b2f366af6fdb7e
dbernhard <dbernhard@ethz.ch>
2023-01-12 12:17:06 +0100

    Fix asinh by converting to floats as doubles have an endless loop

--- src/onnx/asinh.c
+++ src/onnx/asinh.c
@@ -18,11 +18,6 @@ int asinh_baseline(const double* arr, const size_t n, double* result) {
 
 __attribute__((noinline))
 int asinh_ssr(const double* arr, const size_t n, double* result) {
-    register volatile double ft0 asm("ft0");
-    register volatile double ft1 asm("ft1");
-
-    asm volatile("" : "=f"(ft0));
-
     // stream arr into ft0
     snrt_ssr_loop_1d(SNRT_SSR_DM0, n, sizeof(*arr));
     snrt_ssr_repeat(SNRT_SSR_DM0, 1);
@@ -37,12 +32,16 @@ int asinh_ssr(const double* arr, const size_t n, double* result) {
 
     for (size_t i = 0; i < n; i++) {
         asm volatile(
-            "fmv.d fa0, ft0\n" // fa0 <- ft0
+            "fcvt.s.d fa0, ft0\n" // fa0 <- ft0
             ::: "fa0", "ft0"
         );
 
         __builtin_ssr_disable();
 
+        /*
+         * We have to use asinhf as asinh results in an endless loop.
+         * No idea why.
+        */
         asm volatile(
             "call %[add_one]\n"
             :: [add_one] "i"(asinhf)
@@ -56,21 +55,21 @@ int asinh_ssr(const double* arr, const size_t n, double* result) {
         __builtin_ssr_enable();
         
         asm volatile(
-            "fmv.d ft1, fa0" // ft1 <- fa0
+            "fcvt.d.s ft1, fa0" // ft1 <- fa0
             ::: "ft1", "fa0"
         );
     }
 
     snrt_ssr_disable();
-    asm volatile("" :: "f"(ft1));
 
     return 0;
 }
 
 __attribute__((noinline))
 int asinh_ssr_frep(const double* arr, const size_t n, double* result) {
-    // asinh does not benefit from frep
-    // fall back to ssr
-    asinh_ssr(arr, n, result);
+    (void) arr, (void)n, (void) result;
+
+    // Not possible as this needs a function call
+
     return 0;
 }
--- src/onnx/asinh.h
+++ src/onnx/asinh.h
@@ -4,15 +4,11 @@
 
 #include <snrt.h>
 
-#include <math.h>
-
 /*
  * Naive implementation of acosh. Calculates the acosh of n elements starting at arr.
  */
 int ainh_baseline(const double* arr, const size_t n, double* result);
-
 int asinh_ssr(const double* arr, const size_t n, double* result);
-
 int asinh_ssr_frep(const double* arr, const size_t n, double* result);
 
 #endif
